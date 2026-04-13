8d9e78e4b4ec914c2faa1edd20b2f9500d7ee240
art-is-tapesh <tapesh1998@gmail.com>
2026-04-14 03:22:40 +0530

    Update Stripe apiVersion

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { PrismaClient } from "@prisma/client";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2026-03-25.dahlia",
 });
 const prisma = new PrismaClient();
 
